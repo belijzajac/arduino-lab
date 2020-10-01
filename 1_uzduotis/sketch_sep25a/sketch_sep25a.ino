e1104caefba6d57937a0134219ff0268c3a18f66
belijzajac <tautvydas749@gmail.com>
2020-10-01 23:37:13 +0300

    Made buttons control the player on the led display

--- 1_uzduotis/sketch_sep25a/sketch_sep25a.ino
+++ 1_uzduotis/sketch_sep25a/sketch_sep25a.ino
@@ -14,32 +14,65 @@ void setup() {
   Serial.begin(9600);
 
   // https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
-  pinMode(button1Pin, INPUT_PULLUP);
+  pinMode(button1Pin, INPUT);
   attachInterrupt(digitalPinToInterrupt(button1Pin), onYellowBtnPress, FALLING);
-  pinMode(button2Pin, INPUT_PULLUP);
+  pinMode(button2Pin, INPUT);
   attachInterrupt(digitalPinToInterrupt(button2Pin), onRedBtnPress, FALLING);
   
-  lc.shutdown(0, false); // wake up led display
-  lc.setIntensity(0, 1); // set intensity to 1 (0-15)
-  lc.clearDisplay(0);    // clear display
+  lc.shutdown(0, false);    // wake up led display
+  lc.setIntensity(0, 1);    // set intensity to 1 (0-15)
+  lc.clearDisplay(0);       // clear display
+  lc.setLed(0, 3, 0, true); // light up a pixel at (3, 0)
 }
 
+struct Player {
+  int prevRow = 0;
+  int currRow = 3;
+
+  // update player's position
+  void update() {
+    if (currRow != prevRow) {
+      lc.setLed(0, prevRow, 0, false);
+      lc.setLed(0, currRow, 0, true);
+    }
+  }
+
+  // move player "dot" down
+  void increase() {
+    if (currRow + 1 < 8) {
+      prevRow = currRow;
+      ++currRow;
+      update();
+    }
+  }
+
+  // move player "dot" up
+  void decrease() {
+    if (currRow - 1 >= 0) {
+      prevRow = currRow;
+      --currRow;
+      update();
+    }
+  }
+};
+
+struct Player playerDot{};
+
 void loop() {
-  for (int row=0; row < 8; ++row) {
+  /*for (int row=0; row < 8; ++row) {
     for (int col=0; col < 8; ++col) {
       lc.setLed(0, row, col, true);      
       delay(50);
     }
-  }
-
+  }*/
 }
 
 // interrupt function to trigger on the yellow button press
 void onYellowBtnPress() {
-  Serial.println("button1Pin pressed");
+  playerDot.decrease();
 }
 
 // interrupt function to trigger on the red button press
 void onRedBtnPress() {
-  Serial.println("button2Pin pressed");
+  playerDot.increase();
 }
