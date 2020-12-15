7ef52c68b2b1dfde71fd336cba0d6890748d9dbc
belijzajac <tautvydas749@gmail.com>
2020-12-15 08:03:30 +0200

    Working solution

--- 4_uzduotis/sketch_dec11a/sketch_dec11a.ino
+++ 4_uzduotis/sketch_dec11a/sketch_dec11a.ino
@@ -13,7 +13,7 @@ Servo servoObj;
 
 void setup() {
   Serial.begin(9600);
-  
+
   // servo
   servoObj.attach(servoPin);
 
@@ -25,41 +25,53 @@ void setup() {
 
   // setting up the TFT display
   ucg.begin(UCG_FONT_MODE_TRANSPARENT);
+  //ucg.begin(UCG_FONT_MODE_SOLID);
+}
+
+// Didysis apskritimas
+// centro x: 64
+// centro y: 80
+// rad: 60
+void clear_display () {
   ucg.clearScreen();
+  
+  ucg.setFont(ucg_font_ncenR12_tr);
+  ucg.setColor(255, 255, 255);
+  ucg.setColor(1, 255, 0,0);
+  
+  ucg.setColor(255, 255, 255);
+  ucg.drawCircle(64, 80, 60, UCG_DRAW_ALL);
+}
+
+void draw_obstacle (int servoDegrees, int tofDistance) {  
+  ucg.setColor(0, 255, 0);
+  
+  float x_pos = 64 + 60 * cos(servoDegrees * PI / 180) * tofDistance/300;
+  float y_pos = 80 + 60 * sin(servoDegrees * PI / 180) * tofDistance/300;
+  
+  ucg.drawCircle(x_pos, y_pos, 2, UCG_DRAW_ALL);
 }
 
 void measureDistance(int servoPos) {
   VL53L0X_RangingMeasurementData_t result;
   lox.rangingTest(&result, false); // false - don't output debug info
 
-  // if not an error
-  if (result.RangeStatus != 4) {
-    // output to serial: "<angle> <distance>"
-    Serial.print(servoPos);
-    Serial.print(" ");
-    Serial.print(result.RangeMilliMeter);
-    Serial.println();
-  } else {
-    ; // do nothing since we received an error
-      // indicating that we're out of range
+  // if not an error code and within 30 cm
+  if (result.RangeStatus != 4 && result.RangeMilliMeter <= 300) {
+    draw_obstacle(servoPos, result.RangeMilliMeter);
   }
 }
 
 void loop() {
-  //ucg.setRotate90();
-  ucg.setFont(ucg_font_ncenR12_tr);
-  ucg.setColor(255, 255, 255);
-  //ucg.setColor(0, 255, 0);
-  ucg.setColor(1, 255, 0,0);
-  
-  ucg.setPrintPos(0,25);
-  ucg.print("Hello World!");
+  clear_display();
   
   // rotate from 0 to 180 degrees
   for (int pos = 0; pos < 180; ++pos) {
     servoObj.write(pos);
     measureDistance(pos);
   }
+
+  clear_display();
 
   // rotate from 180 to 0 degrees
   for (int pos = 180; pos >= 1; --pos) {
