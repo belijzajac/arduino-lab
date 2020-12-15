fd16f537a41bbf81267e14de6a0fd98527eac758
belijzajac <tautvydas749@gmail.com>
2020-12-15 08:43:34 +0200

    Cleaned up the code

--- 4_uzduotis/sketch_dec11a/sketch_dec11a.ino
+++ 4_uzduotis/sketch_dec11a/sketch_dec11a.ino
@@ -4,18 +4,32 @@
 // for TFT display
 #include <SPI.h>
 #include "Ucglib.h"
-Ucglib_ST7735_18x128x160_SWSPI ucg(/*sclk=*/ 13, /*data=*/ 11, /*cd=*/ 9, /*cs=*/ 10, /*reset=*/ 8);
 
-auto lox = Adafruit_VL53L0X();
+// pins for TFT screen
+const int CLK = 13;
+const int DIN = 11;
+const int DC  = 9;
+const int CS  = 10;
+const int RST = 8;
 
-const int servoPin = 12;
+Ucglib_ST7735_18x128x160_SWSPI ucg(CLK, DIN, DC, CS, RST); // TFT display's object
+auto lox = Adafruit_VL53L0X();                             // ToF sensor's objec
+
+const int SERVO_PIN = 12;
 Servo servoObj;
+
+// constants for ToF
+const int WORKING_DISTANCE = 600; // 300 mm (= 30 cm)
+// constants for the big circle
+const int CIRCLE_CENTER_X = 64;
+const int CIRCLE_CENTER_Y = 80;
+const int CIRCLE_RADIUS   = 60;
 
 void setup() {
   Serial.begin(9600);
 
   // servo
-  servoObj.attach(servoPin);
+  servoObj.attach(SERVO_PIN);
 
   // put to infinite loop if failed to boot CJVL53L0XV2
   if (!lox.begin()) {
@@ -25,14 +39,9 @@ void setup() {
 
   // setting up the TFT display
   ucg.begin(UCG_FONT_MODE_TRANSPARENT);
-  //ucg.begin(UCG_FONT_MODE_SOLID);
 }
 
-// Didysis apskritimas
-// centro x: 64
-// centro y: 80
-// rad: 60
-void clear_display () {
+void clearDisplay () {
   ucg.clearScreen();
   
   ucg.setFont(ucg_font_ncenR12_tr);
@@ -40,14 +49,14 @@ void clear_display () {
   ucg.setColor(1, 255, 0,0);
   
   ucg.setColor(255, 255, 255);
-  ucg.drawCircle(64, 80, 60, UCG_DRAW_ALL);
+  ucg.drawCircle(CIRCLE_CENTER_X, CIRCLE_CENTER_Y, CIRCLE_RADIUS, UCG_DRAW_ALL);
 }
 
-void draw_obstacle (int servoDegrees, int tofDistance) {  
-  ucg.setColor(0, 255, 0);
+void drawObstacle (int servoDegrees, int tofDistance) {  
+  ucg.setColor(0, 255, 0); // green
   
-  float x_pos = 64 + 60 * cos(servoDegrees * PI / 180) * tofDistance/300;
-  float y_pos = 80 + 60 * sin(servoDegrees * PI / 180) * tofDistance/300;
+  float x_pos = CIRCLE_CENTER_X + CIRCLE_RADIUS * cos(servoDegrees * PI / 180) * tofDistance/WORKING_DISTANCE;
+  float y_pos = CIRCLE_CENTER_Y + CIRCLE_RADIUS * sin(servoDegrees * PI / 180) * tofDistance/WORKING_DISTANCE;
   
   ucg.drawCircle(x_pos, y_pos, 2, UCG_DRAW_ALL);
 }
@@ -56,14 +65,14 @@ void measureDistance(int servoPos) {
   VL53L0X_RangingMeasurementData_t result;
   lox.rangingTest(&result, false); // false - don't output debug info
 
-  // if not an error code and within 30 cm
-  if (result.RangeStatus != 4 && result.RangeMilliMeter <= 300) {
-    draw_obstacle(servoPos, result.RangeMilliMeter);
+  // if not an error code and within working distance
+  if (result.RangeStatus != 4 && result.RangeMilliMeter <= WORKING_DISTANCE) {
+    drawObstacle(servoPos, result.RangeMilliMeter);
   }
 }
 
 void loop() {
-  clear_display();
+  clearDisplay();
   
   // rotate from 0 to 180 degrees
   for (int pos = 0; pos < 180; ++pos) {
@@ -71,7 +80,7 @@ void loop() {
     measureDistance(pos);
   }
 
-  clear_display();
+  clearDisplay();
 
   // rotate from 180 to 0 degrees
   for (int pos = 180; pos >= 1; --pos) {
